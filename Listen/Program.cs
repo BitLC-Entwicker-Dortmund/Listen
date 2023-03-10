18e9e118516f49e432d6a44347c20016b325c87d
Klaus <BitLCEntwicklerDortmund@web.de>
2023-03-10 14:36:12 +0100

    Add project files.

---
+++ Listen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace Listen {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, Liste!");
+
+            ArrayList al = new ArrayList();
+            al.Add("Mohamad");
+            al.Add("Matthias");
+            al.Add("Alexander");
+            al.Add("Alexander");
+            al.Add("Ugur");
+
+            foreach (string name in al) {
+                Console.WriteLine( name );
+            }
+
+            Console.Read();
+        }
+    }
+}
